973c6187e6815e2362fa63c872b4dbee57a6cff2
EdrissAria <edrissalokozay77@gmail.com>
2022-09-19 18:14:54 -0700

    delete unused folders

--- backend/validations/userValidation.js
+++
@@ -1,6 +0,0 @@
-const {check} = require('express-validator'); 
-
-exports.signupValidation = [
-    check('username', 'username is required!').not().isEmpty(), 
-    check('password', 'password is required!').not().isEmpty() 
-]
